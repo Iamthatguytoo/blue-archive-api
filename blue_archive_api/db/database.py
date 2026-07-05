72b413d6f626000f5a5ffd7207370553fa23a870
Iamthatguy <84384403+Iamthatguytoo@users.noreply.github.com>
2026-07-05 13:14:58 +0800

    Deleting blue_archive_api directory for new deployment pipeline.

--- blue_archive_api/db/database.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-from db.settings import settings
-
-client = MongoClient(settings.CLIENT)
-db = client[settings.DATABASE]
-
-student_collection = db[settings.API_COLLECTION]
-api_key_collection = db[settings.API_COLLECTION_KEYS]
