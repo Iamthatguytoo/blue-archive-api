6fd287b0c68a8a82c4391e8e0fd2b291e6ba4936
Iamthatguytoo <almazan.jash.andrei@gmail.com>
2026-07-05 13:24:52 +0800

    Removed comment

--- db/database.py
+++ db/database.py
@@ -6,25 +6,3 @@ db = client[settings.DATABASE]
 
 student_collection = db[settings.API_COLLECTION]
 api_key_collection = db[settings.API_COLLECTION_KEYS]
-
-"""
-from pathlib import Path
-from dotenv import load_dotenv
-
-env_path = Path(__file__).resolve().parent.parent / ".env.local" 
-
-load_dotenv(env_path, override=True)
-
-env_client = os.getenv("CLIENT")
-env_db = os.getenv("DATABASE")
-env_collection = os.getenv("API_COLLECTION")
-env_collection_keys = os.getenv("API_COLLECTION_KEYS")
-
-client = MongoClient(env_client)
-db = client[env_db]
-student_collection = db[env_collection]
-api_key_collection = db[env_collection_keys]
-
-def get_db():
-    return db
-"""
